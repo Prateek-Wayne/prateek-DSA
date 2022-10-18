c00dfccf9a32c4ba5d420196bca5b818628906b0
Prateek Verma <prateek.12016050@lpu.in>
2022-10-18 16:19:23 +0000

    Sync LeetCode submission - Nth Highest Salary (mysql)

---
+++ my-folder/problems/nth_highest_salary/solution.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+      select distinct(salary) from Employee e1 
+      where
+      N-1=(select count(distinct(salary)) from Employee e2 
+                       where e2.salary>e1.salary)
+  );
+END
