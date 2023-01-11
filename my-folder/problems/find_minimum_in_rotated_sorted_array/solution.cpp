e9d11734906ae37abaec0236a114f71b240267fc
Prateek Verma <prateek.12016050@lpu.in>
2023-01-11 13:57:51 +0000

    Sync LeetCode submission - Find Minimum in Rotated Sorted Array (cpp)

--- my-folder/problems/find_minimum_in_rotated_sorted_array/solution.cpp
+++ my-folder/problems/find_minimum_in_rotated_sorted_array/solution.cpp
@@ -2,32 +2,27 @@ class Solution {
 public:
     int findMin(vector<int>& arr) {
         
+         int start = 0;
+    int end = arr.size() - 1;
+    if (arr[start] <= arr[end])
+        return arr[start];
+int n=arr.size();
+    while (start <= end)
+    {
+
+        int mid = start + (end - start) / 2;
         
-        int low=0;
-        int N=arr.size();
-        int high=N-1;
-        // int mid=0;
-        if(arr[low]<=arr[high])
-            return arr[low];
-        while(low<=high)
-        {
-             int mid=low+(high-low)/2;
-            int next=(mid+1)%N;
-            int prev=(mid+N-1)%N;
-            
-            if(arr[mid]<=arr[prev] && arr[mid]<=arr[next])
-                return arr[mid];
-            else if(arr[low]<=arr[high])
-                return arr[low];
-            else if(arr[low]<=arr[mid])
-                low=mid+1;
-            // else if(arr[mid]<=arr[high])
-            //     high=mid-1;
-            else 
-                high=mid-1;
-           
-        }
-        return arr[0];
-        
+        int prev = (mid +n- 1)%n;
+        int next = (mid + 1)%n;
+        if (arr[prev] >= arr[mid] && arr[next] >= arr[mid])
+            return arr[mid];
+        else if (arr[start] <= arr[end])
+            return arr[start];
+        else if (arr[start] <= arr[mid])
+            start = mid + 1;
+        else if (arr[mid] <= arr[end])
+            end = mid - 1;
+    }
+    return 0;
     }
 };
