ac4792c2a58933ba228dee7ebe02260320e19881
Prateek Verma <prateek.12016050@lpu.in>
2024-06-24 15:28:41 +0000

    Sync LeetCode submission - Maximum Subarray (cpp)

--- my-folder/problems/maximum_subarray/solution.cpp
+++ my-folder/problems/maximum_subarray/solution.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        
+        int maxy=INT_MIN;
         int sum=0;
-        int maxi=INT_MIN;
         for(int i=0;i<nums.size();i++)
         {
             sum+=nums[i];
-            maxi=max(maxi,sum);
+            maxy=max(maxy,sum);
             if(sum<0)
+            {
                 sum=0;
+            }
         }
-        return maxi;
+        return maxy;
     }
 };
