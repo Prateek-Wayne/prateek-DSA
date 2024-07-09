499a6c011c0ed8a662b61e366c1b43880229a6b0
Prateek Verma <prateek.12016050@lpu.in>
2024-07-09 03:59:47 +0000

    Sync LeetCode submission - Find the Duplicate Number (cpp)

--- my-folder/problems/find_the_duplicate_number/solution.cpp
+++ my-folder/problems/find_the_duplicate_number/solution.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-        map<int,int> m;
-        for(int i=0;i<nums.size();i++)
-            m[nums[i]]++;
-        for(auto i:m)
+    int findDuplicate(vector<int>& arr) {
+        
+        int N = arr.size();
+    int i=0;
+    while(i<N)
+    {
+        if(arr[i]!=arr[arr[i]-1])
         {
-            if(i.second>1)
-                return i.first;
+            swap(arr[i],arr[arr[i]-1]);
+            // i++;
         }
+        else if(arr[i]==arr[arr[i]-1])
+            i++;
+    }
+
+    for(int i=0;i<N;i++)
+    {
+        if(arr[i]!=i+1)
+            return arr[i];
+    }
         return -1;
     }
 };
