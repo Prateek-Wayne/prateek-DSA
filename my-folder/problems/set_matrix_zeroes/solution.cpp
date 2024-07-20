3228b9c68e6503d461255c8aaef77281e1ccb10c
Prateek Verma <prateek.12016050@lpu.in>
2024-07-20 04:44:55 +0000

    Sync LeetCode submission - Set Matrix Zeroes (cpp)

--- my-folder/problems/set_matrix_zeroes/solution.cpp
+++ my-folder/problems/set_matrix_zeroes/solution.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-void setZeroes(std::vector<std::vector<int>>& matrix) {
-    std::vector<std::vector<int>> store;
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+    int m = matrix[0].size();
+    vector<int> row( n,1);
+    vector<int> cols( m,1);
 
-    // Collect positions of zeroes
-    for (int i = 0; i < matrix.size(); i++) {
-        for (int j = 0; j < matrix[i].size(); j++) {
-            if (matrix[i][j] == 0) {
-                store.push_back({i, j});
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                row[i] = 0;
+                cols[j] = 0;
             }
         }
     }
-    for(auto i:store)
-    {   // row 
-        int row=i[0];
-        for(int j=0;j<matrix[row].size();j++)
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
         {
-            matrix[row][j]=0;
+            if (row[i] == 0 || cols[j] == 0)
+            {
+                matrix[i][j]=0;
+            }
         }
-        //columns 
-        int col=i[1];
-for(int j=0;j<matrix.size();j++)
-{
-    matrix[j][col]=0;
-}
     }
-
-    // for(auto i:matrix)
-    // {
-    //     for(auto j:i)
-    //     {
-    //         cout<<j<<"|";
-    //     }
-    //     cout<<endl;
-    // }
-}
+    }
 };
