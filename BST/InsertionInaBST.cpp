b23013c3d94771e04e45eb816737c22f35b079a9
Prateek-Wayne <lordprateekverma@gmail.com>
2024-09-08 10:33:55 +0530

    tree

--- BST/InsertionInaBST.cpp
+++ BST/InsertionInaBST.cpp
@@ -10,35 +10,70 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+int count=0;
+ int kthSmallest(TreeNode* root, int &k) {
+        if(root!=NULL)
+        {
+            return kthSmallest(root->left,k);
+            k--;
+            if(k==0)
+                return root->val;
+            return kthSmallest(root->right,k);
+        }
+        return -1;
+    }
+
 TreeNode *insertIntoBST(TreeNode *root, int key)
 {
-    TreeNode ans = *new TreeNode();
-    while (root != NULL)
+    if (root == NULL)
     {
-        if(root->val<=key)
-        {
-            ans=max(ans.val,root->val);
-        }
-        root = root->val >= key ? root->left : root->right;
+        return new TreeNode(key);
     }
-    TreeNode *temp=new TreeNode();
-    temp->right=ans.right;
-    ans.right=temp;
-   temp->val=key;
-    return root;
 
+    TreeNode *current = root;
+    while (true)
+    {
+        if (key < current->val)
+        {
+            if (current->left == NULL)
+            {
+                current->left = new TreeNode(key);
+                break;
+            }
+            else
+            {
+                current = current->left;
+            }
+        }
+        else
+        {
+            if (current->right == NULL)
+            {
+                current->right = new TreeNode(key);
+                break;
+            }
+            else
+            {
+                current = current->right;
+            }
+        }
+    }
+    return root;
 }
 
-void inorderTraversal(TreeNode* root) {
-    if (root == nullptr) return;
+void inorderTraversal(TreeNode *root)
+{
+    if (root == NULL)
+        return;
     inorderTraversal(root->left);
     cout << root->val << " ";
     inorderTraversal(root->right);
 }
 
-int main() {
+int main()
+{
     // Create a simple BST
-    TreeNode* root = new TreeNode(4);
+    TreeNode *root = new TreeNode(4);
     root->left = new TreeNode(2);
     root->right = new TreeNode(7);
     root->left->left = new TreeNode(1);
---
+++ BST/LCA.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+{
+    if(root!=NULL)
+    {
+        if(root->val>p->val && root->val >q->val)
+        {
+            return lowestCommonAncestor(root->left,p,q);
+        }
+        else if(root->val<p->val && root->val<q->val)
+            return lowestCommonAncestor(root->right,p,q);
+        return root;
+    }
+    return root;
+}
