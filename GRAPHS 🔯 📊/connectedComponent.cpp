715377c6ef021cdfec8a47cd4bfbb0442f1f5eaa
Prateek-Wayne <lordprateekverma@gmail.com>
2024-09-09 08:36:00 +0530

    sd

--- GRAPHS 🔯 📊/connectedComponent.cpp
+++ GRAPHS 🔯 📊/connectedComponent.cpp
@@ -1,14 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void BFS(vector<int> adj[], vector<int> &Visited, int Node)
+void BFS(vector<vector<int>> &adj, vector<int> &Visited, int Node)
 {
     queue<int> q;
     q.push(Node);
+    Visited[Node] = 1;
+
     while (!q.empty())
     {
-        auto it = q.front();
+        int it = q.front();
         q.pop();
+
         for (auto i : adj[it])
         {
             if (Visited[i] == 0)
@@ -19,30 +22,37 @@ void BFS(vector<int> adj[], vector<int> &Visited, int Node)
         }
     }
 }
+
 void solve(int A, vector<vector<int>> &B)
 {
-    vector<int> adj[A];
-    int count=0;
+    vector<vector<int>> adj(A); // Correctly initialize the adjacency list
+    int count = 0;
 
+    // Build the adjacency list
     for (auto i : B)
     {
         adj[i[0]].push_back(i[1]);
         adj[i[1]].push_back(i[0]);
     }
-    vector<int> Visited(A,0);
-    for(int i=0;i<adj->size();i++)
+
+    vector<int> Visited(A, 0); // Initialize visited array to 0
+
+    for (int i = 0; i < A; i++)
     {
-        if(Visited[i]==0)
+        if (Visited[i] == 0)
         {
             count++;
-            BFS(adj,Visited,i);
+            BFS(adj, Visited, i);
         }
     }
-    cout<<count;
+
+    cout << count << endl;
 }
-int main()
-{
-    int A = 4;
-    vector<vector<int>> B = {{1, 2}, {2, 3}};
+
+int main() {
+    int A = 100;
+    vector<vector<int>> B = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 5}}; // Example input
     solve(A, B);
+
+    return 0;
 }
