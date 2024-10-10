e882b1aa1438a378ee78c6486176cc92f1b8c2c6
Prateek-Wayne <lordprateekverma@gmail.com>
2024-10-10 08:41:31 +0530

    ,bsjkbc

--- recursion/CountsubarryaWithGIvenSum.cpp
+++ recursion/CountsubarryaWithGIvenSum.cpp
@@ -20,15 +20,15 @@ void helper(vector<int> & nums,int k,int index,vector<int> &ds,int &sum,int &cou
     helper(nums,k,index+1,ds,sum,count);
 }
 
-int subarraySum(vector<int> &nums, int k)
-{
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    // Write your code here.
     int sum=0;
     vector<int> ds;
     int count=0;
-
-    helper(nums,k,0,ds,sum,count);
-    return count;
-
+    helper(arr,k,0,ds,sum,count);
+    if(count>0)
+        return true;
+    return false;
 }
 int main()
 {
