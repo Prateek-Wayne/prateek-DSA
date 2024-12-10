ddb8606154c9ceba468bfcbe959be664f1d61b80
Prateek Verma <lordprateekverma@gmail.com>
2024-12-10 18:34:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Geek Jump/geek-jump.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+int helper(vector<int> &height, int n, vector<int> &dp)
+{
+    if (n == 0)
+        return 0;
+    if (dp[n] != -1)
+        return dp[n];
+
+    int left = helper(height, n - 1, dp) + abs(height[n] - height[n - 1]);
+    int right = INT_MAX;
+    if (n > 1)
+        right = helper(height, n - 2, dp) + abs(height[n] - height[n - 2]);
+
+    return dp[n] = min(left, right);
+}
+
+int minimumEnergy(vector<int> &height, int n)
+{
+    // Code here
+    vector<int> dp(n,-1);
+    return helper(height, n-1,dp);
+}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.minimumEnergy(arr, N) << "\n";
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends
