0c37191e3c4c5883fdb20e7a3bb6ca20c804d0ad
Prateek-Wayne <lordprateekverma@gmail.com>
2024-12-10 20:23:55 +0530

    best

--- Dynamic Programming/FrogJump.cpp
+++ Dynamic Programming/FrogJump.cpp
@@ -1,31 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int helper(vector<int> &height, int n)
+int helper(vector<int> &height,int n) // n is 0 based n-1...
 {
-    if (n == 0)
+    if(n==0)
         return 0;
-    if(n==1)
-        return height[n-1];
-  
+    
+    int left =helper(height,n-1)+abs( height[n]-height[n-1]);
+    int right=INT_MAX;
+    if(n>1)
+    {
+        right=helper(height,n-2)+abs(height[n]-height[n-2]);
+    }
+    return min(left,right);
 
-    int left = helper(height, n - 1) + abs(height[n] - height[n - 1]);
-
-        int right = helper(height, n - 2) + abs(height[n] - height[n - 2]);
-
-    return min(left, right);
 }
 
 int minimumEnergy(vector<int> &height, int n)
 {
     // Code here
-    return helper(height, n-1);
+    return helper(height,n);
+    
 }
 
 int main()
 {
-    vector<int> height = {10, 20, 30, 10};
-    int n = 4;
+    vector<int> height = {10, 30, 40};
+    int n = 3;
     cout << minimumEnergy(height, n);
     return 0;
 }
