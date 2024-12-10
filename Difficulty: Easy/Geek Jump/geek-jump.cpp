a6a60294030f20a6c1622958ae97bf405a7a17a1
Prateek Verma <lordprateekverma@gmail.com>
2024-12-10 21:05:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Geek Jump/geek-jump.cpp
+++ Difficulty: Easy/Geek Jump/geek-jump.cpp
@@ -7,26 +7,31 @@ using namespace std;
 class Solution {
   public:
 
-int helper(vector<int> &height, int n, vector<int> &dp)
+int helper(vector<int> &height,int n,vector<int> &dp) // n is 0 based n-1...
 {
-    if (n == 0)
+    if(n==0)
+    {   
+        dp[0]=0;
         return 0;
-    if (dp[n] != -1)
+    }
+    if(dp[n]!=-1)
         return dp[n];
+    int left =helper(height,n-1,dp)+abs( height[n]-height[n-1]);
+    int right=INT_MAX;
+    if(n>1)
+    {
+        right=helper(height,n-2,dp)+abs(height[n]-height[n-2]);
+    }
+    return dp[n]=min(left,right);
 
-    int left = helper(height, n - 1, dp) + abs(height[n] - height[n - 1]);
-    int right = INT_MAX;
-    if (n > 1)
-        right = helper(height, n - 2, dp) + abs(height[n] - height[n - 2]);
-
-    return dp[n] = min(left, right);
 }
 
 int minimumEnergy(vector<int> &height, int n)
 {
     // Code here
-    vector<int> dp(n,-1);
-    return helper(height, n-1,dp);
+    vector<int> dp(n+1,-1);
+    return helper(height,n-1,dp);
+    
 }
 };
 
