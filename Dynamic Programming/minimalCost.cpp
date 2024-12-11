eaf9add2a17e5ff9af3c7949f8769c14b1539fdf
Prateek-Wayne <lordprateekverma@gmail.com>
2024-12-11 08:17:46 +0530

    h

--- Dynamic Programming/minimalCost.cpp
+++ Dynamic Programming/minimalCost.cpp
@@ -1,35 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int helper(int n,int k,vector<int> &height)
+int helper(int n, int k, vector<int> &height)
 {
-    if(n==0)
+    if (n == 0)
         return 0;
-    int left=helper(n-1,k,height)+abs(height[n]-height[n-1]);
-    int right=INT_MAX;
-    for(int i=2;i<=k;i++)
+    int ans = INT_MAX;
+    for (int i = 1; i <= k; i++)
     {
-        if(n>=i)
+        int jump = INT_MAX;
+        if (n >= i)
         {
-            right=helper(n-i,k,height)+abs(height[n]-height[n-i]);
-
+            int jump = helper(n - i, k, height) + abs(height[n] - height[n - i]);
+            ans = min(ans, jump);
         }
     }
-    return min(left,right);
-
+    return ans;
 }
 
 int minimizeCost(int k, vector<int> &arr)
 {
-    return helper(arr.size()-1,k,arr);
-
+    return helper(arr.size() - 1, k, arr);
 }
 int main()
 {
-    //  k = 3, arr[]= [10, 30, 40, 50, 20]
+    vector<int> arr = {96, 48, 27, 72, 39, 70, 13, 68, 100};
+    int k = 7;
     // vector<int> arr = {10, 30, 40, 50, 20};
-    vector<int> arr = {10,20,10};
-    int k = 1;
+    // vector<int> arr = {10,20,10};
+    // int k = 1;
     cout << minimizeCost(k, arr) << endl;
     return 0;
 }
