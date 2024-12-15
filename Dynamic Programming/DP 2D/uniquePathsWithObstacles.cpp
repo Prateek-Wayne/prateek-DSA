9a7e44a92c0f070224dc568ac8b724d760d328d9
Prateek-Wayne <lordprateekverma@gmail.com>
2024-12-15 16:35:01 +0530

    done

--- Dynamic Programming/DP 2D/uniquePathsWithObstacles.cpp
+++ Dynamic Programming/DP 2D/uniquePathsWithObstacles.cpp
@@ -1,23 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int helper(vector<vector<int>>& obstacleGrid,int R,int C)
+int helper(vector<vector<int>>& obstacleGrid,int R,int C,vector<vector<int>> &dp)
 {
     if(R==obstacleGrid.size()-1 && C==obstacleGrid[0].size()-1 && obstacleGrid[R][C]!=1)
     {
+        dp[R][C]=1;
         return 1;
     }
+    if(dp[R][C]!=-1)
+        return dp[R][C];
     int path=0;
     if(R+1<obstacleGrid.size())
     {   
         if(obstacleGrid[R][C]!=1)
-            path+=helper(obstacleGrid,R+1,C);
+            path+=helper(obstacleGrid,R+1,C,dp);
     }
     if(C+1<obstacleGrid[0].size())
     {
         if(obstacleGrid[R][C]!=1)
-            path+=helper(obstacleGrid,R,C+1);
+            path+=helper(obstacleGrid,R,C+1,dp);
     }
+    dp[R][C]=path;
     return path;
 
 }
@@ -25,7 +29,9 @@ int helper(vector<vector<int>>& obstacleGrid,int R,int C)
 int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
     int R=0;
     int C=0;
-        return helper(obstacleGrid,R,C);
+    vector<vector<int>> dp(obstacleGrid.size(),vector<int> (obstacleGrid[0].size(),-1));
+
+        return helper(obstacleGrid,R,C,dp);
     }
 
 int main()
