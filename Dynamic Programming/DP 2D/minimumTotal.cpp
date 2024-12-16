f8d8c084c9073abfba2dc38f44ee6ae4911cc196
Prateek-Wayne <lordprateekverma@gmail.com>
2024-12-16 08:34:41 +0530

    d

--- Dynamic Programming/DP 2D/minimumTotal.cpp
+++ Dynamic Programming/DP 2D/minimumTotal.cpp
@@ -8,31 +8,23 @@ int helper(vector<vector<int>> &triangle, int R, int C)
         return triangle[R][C];
     }
 
-    int left = INT_MAX;
-    int right = INT_MAX;
-    if (R < triangle.size() - 1)
-    {
-
-        left = helper(triangle, R + 1, C) + triangle[R][C];
-
-        if (R < triangle.size() - 1 && C + 1 < triangle[R].size() - 1)
-        {
-            right = helper(triangle, R + 1, C + 1) + triangle[R][C];
-        }
-    }
-    return min(left, right);
+    int left=triangle[R][C]+helper(triangle,R+1,C);
+    int right=triangle[R][C]+helper(triangle,R+1,C+1);
+    return min(left,right);
 }
 
 int minimumTotal(vector<vector<int>> &triangle)
 {
     int R = 0;
     int C = 0;
+    
     return helper(triangle, R, C);
 }
 int main()
 {
     // vector<vector<int>> triangle = {{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}};
-    vector<vector<int>> triangle ={{-10}};
+    // vector<vector<int>> triangle ={{-10}};
+    vector<vector<int>> triangle ={{-1},{-2,-3}};
     cout << minimumTotal(triangle) << endl;
     return 0;
 }
