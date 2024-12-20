01920ad8fcf14c9df8b5e2e03fe9196a05f8fdbd
Prateek-Wayne <lordprateekverma@gmail.com>
2024-12-20 18:25:49 +0530

    sd

--- Dynamic Programming/DP 2D/GeekNinja.cpp
+++ Dynamic Programming/DP 2D/GeekNinja.cpp
@@ -1,51 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int helper(vector<vector<int>> &arr, int last, int days, vector<vector<int>> &dp)
-{ // base...
-    if (days == 0)
+int helper(vector<vector<int>> &arr, int d, int last)
+{
+    // base..
+    if (d == 0)
     {
         int temp = INT_MIN;
         for (int i = 0; i < arr[0].size(); i++)
         {
-
             if (i != last)
             {
-                temp = max(temp, arr[days][i]);
-                dp[0][i] = temp;
+                temp = max(temp, arr[d][i]);
             }
         }
         return temp;
     }
-   if (dp[days][last] != -1&& last< arr[0].size())
-        return dp[days][last];
     int ans = INT_MIN;
     for (int i = 0; i < arr[0].size(); i++)
     {
+
         if (i != last)
         {
-            int temp = helper(arr, i, days - 1, dp) + arr[days][i];
+            int temp = helper(arr, d - 1, i) + arr[d][i];
             ans = max(ans, temp);
         }
     }
-    dp[days][last] = ans;
     return ans;
 }
 
 int maximumPoints(vector<vector<int>> &arr, int n)
 {
-    vector<vector<int>> dp(n, vector<int>(arr[0].size(), -1));
-    return helper(arr, arr[0].size(), n - 1, dp);
+    // Code here
+    int days = arr.size() - 1;
+    int last = arr[0].size();
+    return helper(arr, days, last);
 }
 int main()
 {
-
     // Input: n=3 and arr[]= [[1,2,5],[3,1,1],[3,3,3]]
-    // vector<vector<int>> arr = {{1, 2, 5}, {3, 1, 1}, {3, 3, 3}};
+    vector<vector<int>> arr = {{1, 2, 5}, {3, 1, 1}, {3, 3, 3}};
     // 2,1,6| 3,4,6  | 10,1,6 | 8,3,7
-    vector<vector<int>> arr = {{2, 1, 3}, {3, 4, 6}, {10, 1, 6}, {8, 3, 7}};
-    int n = 4;
+    // vector<vector<int>> arr = {{2, 1, 3}, {3, 4, 6}, {10, 1, 6}, {8, 3, 7}};
+    int n = 3;
     cout << maximumPoints(arr, n) << endl;
-
     return 0;
 }
--- Dynamic Programming/DP 2D/houseRObber2.cpp
+++ Dynamic Programming/DP 2D/houseRObber2.cpp
@@ -17,17 +17,44 @@ int helper(vector<int> &nums, int index,vector<int> &dp)
 
 int rob(vector<int> &nums)
 {   int n=nums.size()-1;
-    vector<int> dp1(n+1,-1);
-    vector<int> dp2(n+1,-1);
-    vector<int> first;
-    vector<int> second;
-    first.assign(nums.begin(),nums.end()-1);
-    second.assign(nums.begin()+1,nums.end());
-    return max(helper(first,n-1,dp1),helper(second,n-1,dp2));
+    vector<int> first(nums.begin(),nums.end()-1);
+    if(n==0)
+        return nums[0];
+    vector<int> second(nums.begin()+1,nums.end());
+
+     vector<int> dp1(first.size(),-1);
+    vector<int> dp2(second.size(),-1);
+    
+    dp1[0]=first[0];
+
+    dp2[0]=second[0];
+    for(int i=1;i<first.size();i++)
+    {
+        int pick=first[i];
+        if(i>1)
+            pick+=dp1[i-2];
+        int notpick=dp1[i-1]+0;
+
+        dp1[i]=max(pick,notpick);
+        
+    }
+
+     for(int i=1;i<second.size();i++)
+    {
+        int pick=second[i];
+        if(i>1)
+            pick+=dp2[i-2];
+        int notpick=dp2[i-1]+0;
+
+        dp2[i]=max(pick,notpick);  
+    }
+    return max(dp1[dp1.size()-1],dp2[dp2.size()-1]);
+   
 }
 int main()
 {   
-    vector<int> nums={1};
+    // vector<int> nums={2,3,2};
+    vector<int> nums={2};
     cout<<rob(nums);
     return 0;
 }
