5ccc26a36f18084e6d9c2dc6e0588a6208f8b533
Prateek-Wayne <lordprateekverma@gmail.com>
2024-12-20 19:24:41 +0530

    as

--- Dynamic Programming/DP 2D/uniquePath.cpp
+++ Dynamic Programming/DP 2D/uniquePath.cpp
@@ -1,37 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int helper(int R,int C,int m ,int n,vector<vector<int>> &dp)
+int helper(int row,int col)
 {
-    if(R==m && C==n)
-    {
-        dp[R][C]=1;
+    if(row==0 && col==0)
         return 1;
-    }
-    if(dp[R][C]!=-1)
-        return dp[R][C];
-    int path=0;
-    if(R+1<=m)
-    {  
-        path+=helper(R+1,C,m,n,dp);
-        dp[R][C]=path;
-    }
-    if(C+1<=n)
-    {
-        path+=helper(R,C+1,m,n,dp);
-        dp[R][C]=path;
-    }
-    return path;
+    if(row<0 || col<0)
+        return 0;
+    
+    int left=helper(row,col-1);
+    int right=helper(row-1,col);
+
+    return left+right;
 }
 
 int uniquePaths(int m, int n)
 {
-    vector<vector<int>> dp(m, vector<int>(n, -1));
-    return helper(0, 0, m - 1, n - 1,dp);
+    int row=m-1;
+    int col=n-1;
+    return helper(row,col);
+
 }
 
 int main()
 {   
-    cout<< uniquePaths(3,7);
+    cout<< uniquePaths(3,2);
     return 0;
 }
