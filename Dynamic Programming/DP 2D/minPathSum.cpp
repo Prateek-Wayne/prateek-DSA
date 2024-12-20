5b78ca7e5fa63f73d0a2d1bf7e7b0cf2cfe3c41d
Prateek-Wayne <lordprateekverma@gmail.com>
2024-12-20 20:49:41 +0530

    sd

--- Dynamic Programming/DP 2D/minPathSum.cpp
+++ Dynamic Programming/DP 2D/minPathSum.cpp
@@ -1,40 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int helper(vector<vector<int>> &grid,int R,int C,vector<vector<int>> &dp)
+int helper(vector<vector<int>> &grid, int row, int col, vector<vector<int>> &dp)
 {
-    if(R==grid.size()-1 && C==grid[0].size()-1)
-    {
-        dp[R][C]=grid[R][C];
-        return grid[R][C];
-    }
+    if (row == 0 && col == 0)
+        return grid[row][col];
 
-    if(dp[R][C]!=-1)
-        return dp[R][C];
-    int left=INT_MAX;
-    if(R<grid.size()-1)
-    {
-        left=helper(grid,R+1,C,dp)+grid[R][C];
-    }
-    int right=INT_MAX;
-    if(C<grid[0].size()-1)
-    {
-        right=helper(grid,R,C+1,dp)+grid[R][C];
-    }
-    return dp[R][C]= min(left,right);
+    if (row < 0 || col < 0)
+        return INT_MAX - 10000;
+
+    if (dp[row][col] != -1)
+        return dp[row][col];
+
+    int left = helper(grid, row, col - 1, dp) + grid[row][col];
+    int up = helper(grid, row - 1, col, dp) + grid[row][col];
+    return dp[row][col] = min(left, up);
 }
 
 int minPathSum(vector<vector<int>> &grid)
 {
-    int R=0;
-    int C=0;
-    vector<vector<int>> dp(grid.size(),vector<int> (grid[0].size(),-1));
-    return helper(grid,R,C,dp);
+    int row = grid.size() - 1;
+    int col = grid[0].size() - 1;
+    vector<vector<int>> dp(row + 1, vector<int>(col + 1, -1));
+    return helper(grid, row, col, dp);
 }
 int main()
 {
-    vector<vector<int>> grid = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
-    // vector<vector<int>> grid = {{1, 2, 3}, {4, 5, 6}};
+    // vector<vector<int>> grid = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+    vector<vector<int>> grid = {{1, 2, 3}, {4, 5, 6}};
     cout << minPathSum(grid) << endl;
     return 0;
 }
