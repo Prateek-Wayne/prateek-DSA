b21975ecce367349cc18badfe6df1ce7e3ba3962
Prateek-Wayne <lordprateekverma@gmail.com>
2024-12-20 19:53:48 +0530

    w

--- Dynamic Programming/DP 2D/uniquePathsWithObstacles.cpp
+++ Dynamic Programming/DP 2D/uniquePathsWithObstacles.cpp
@@ -1,43 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int helper(vector<vector<int>>& obstacleGrid,int R,int C,vector<vector<int>> &dp)
+int helper(vector<vector<int>> &obstacleGrid, int row, int col, vector<vector<int>> &dp)
 {
-    if(R==obstacleGrid.size()-1 && C==obstacleGrid[0].size()-1 && obstacleGrid[R][C]!=1)
-    {
-        dp[R][C]=1;
-        return 1;
-    }
-    if(dp[R][C]!=-1)
-        return dp[R][C];
-    int path=0;
-    if(R+1<obstacleGrid.size())
-    {   
-        if(obstacleGrid[R][C]!=1)
-            path+=helper(obstacleGrid,R+1,C,dp);
-    }
-    if(C+1<obstacleGrid[0].size())
-    {
-        if(obstacleGrid[R][C]!=1)
-            path+=helper(obstacleGrid,R,C+1,dp);
-    }
-    dp[R][C]=path;
-    return path;
+    if (row == 0 && col == 0 && obstacleGrid[row][col] != 1)
+        return dp[row][col] = 1;
+    if (row < 0 || col < 0 || obstacleGrid[row][col] == 1)
+        return 0;
+    if (dp[row][col] != -1)
+        return dp[row][col];
 
+    int left = helper(obstacleGrid, row, col - 1, dp);
+    int up = helper(obstacleGrid, row - 1, col, dp);
+
+    return dp[row][col] = left + up;
 }
 
-int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-    int R=0;
-    int C=0;
-    vector<vector<int>> dp(obstacleGrid.size(),vector<int> (obstacleGrid[0].size(),-1));
-
-        return helper(obstacleGrid,R,C,dp);
-    }
-
+int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
+{
+    int row = obstacleGrid.size() - 1;
+    int col = obstacleGrid[0].size() - 1;
+    vector<vector<int>> dp(row + 1, vector<int>(col + 1, -1));
+    return helper(obstacleGrid, row, col, dp);
+}
 int main()
 {
-    vector<vector<int>> obstacleGrid = {{0,0,0},{0,1,0},{0,0,0}};
-    // vector<vector<int>> obstacleGrid =  {{0,0},{0,1}};
+    // vector<vector<int>> obstacleGrid = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+    vector<vector<int>> obstacleGrid = {{1}};
     cout << uniquePathsWithObstacles(obstacleGrid) << endl;
- return 0;
+    return 0;
 }
