a4551e7d1a9e2e87f5466d2777b1203d55a91e66
Prateek Verma <lordprateekverma@gmail.com>
2024-12-25 11:50:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -28,13 +28,37 @@ bool helper(vector<int> &arr, int target, int ind, vector<vector<int>> &dp)
     return dp[ind][target] = pick || notpick;
 }
 
+// bool isSubsetSum(vector<int> &arr, int target)
+// {
+//     // code here
+//     int n = arr.size() - 1;
+//     vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
+
+//     return helper(arr, target, n, dp);
+// }
 bool isSubsetSum(vector<int> &arr, int target)
 {
     // code here
-    int n = arr.size() - 1;
-    vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
+   int n = arr.size() - 1;
+    vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+    for (int i = 0; i <= n; i++)
+    {
+        dp[i][0] = true;
+    }
+    dp[0][arr[0]] = true;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= target; j++)
+        {
+            bool notpick = dp[i - 1][j];
 
-    return helper(arr, target, n, dp);
+            bool pick = false;
+            if (arr[i] <= j)
+                pick = dp[i - 1][j - arr[i]];
+            dp[i][j] = pick || notpick;
+        }
+    }
+    return dp[n][target];
 }
 };
 
