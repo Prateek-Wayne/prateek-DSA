90a16f813ec8f878d68a717a337ef5006f0b4666
Prateek-Wayne <lordprateekverma@gmail.com>
2024-12-29 12:13:52 +0530

    d

--- Dynamic Programming/DPonSUbSequence/PrintingLCS.cpp
+++ Dynamic Programming/DPonSUbSequence/PrintingLCS.cpp
@@ -41,34 +41,26 @@ int getLCSLength(string &s1, string &s2)
     }
     string ans(dp[ind1][ind2], '$');
     int index = ans.size() - 1;
+    int i = ind1;
+    int j = ind2;
 
-    for (int temp = 1; temp <= ind2; temp++)
+    while (i > 0 && j > 0)
     {
-        int i = ind1;
-        int j = temp;
-        if (dp[ind1][temp] == dp[ind1][ind2])
+        if (s1[i] == s2[j])
         {
-
-            while (i > 0 && j > 0)
-            {
-                if (s1[i] == s2[j])
-                {
-                    ans[index] = s1[i - 1];
-                    index--;
-                    i--;
-                    j--;
-                }
-                else if (dp[i][j - 1] > dp[i - 1][j])
-                {
-                    j--;
-                }
-                else
-                    i--;
-            }
-            cout << ans << endl;
+            ans[index] = s1[i - 1];
+            index--;
+            i--;
+            j--;
         }
+        else if (dp[i][j - 1] > dp[i - 1][j])
+        {
+            j--;
+        }
+        else
+            i--;
     }
-
+    cout << ans << endl;
     // printing the array for now
     return dp[ind1][ind2];
 }
---
+++ Dynamic Programming/DPonSUbSequence/shortestSuperSeq.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+string shortestCommonSupersequence(string s1, string s2)
+{
+
+    int ind1 = s1.length();
+    int ind2 = s2.length();
+    vector<vector<int>> dp(ind1 + 1, vector<int>(ind2 + 1, -1));
+    for (int i = 0; i <= ind1; i++)
+    {
+        dp[i][0] = 0;
+    }
+    for (int j = 0; j <= ind2; j++)
+        dp[0][j] = 0;
+
+    for (int i = 1; i <= ind1; i++)
+    {
+        for (int j = 1; j <= ind2; j++)
+        {
+            if (s1[i - 1] == s2[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    int lcs = dp[ind1][ind2];
+    string sequence(lcs, '$');
+    int index = lcs - 1;
+    int i = ind1;
+    int j = ind2;
+    while (i > 0 && j > 0)
+    {
+        if (s1[i - 1] == s2[j - 1])
+        {
+            sequence[index] = s1[i - 1];
+            index--;
+            i--;
+            j--;
+        }
+        else if (dp[i] > dp[j])
+        {
+            j--;
+        }
+        else
+            i--;
+    }
+
+    return sequence;
+}
+
+int main()
+{
+    string s1 = "abac";
+    string s2 = "cab";
+    cout << shortestCommonSupersequence(s1, s2);
+    return 0;
+}
