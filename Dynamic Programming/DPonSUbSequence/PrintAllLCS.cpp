b6440987a3918ce3bab874d144d90e05017a46eb
Prateek-Wayne <lordprateekverma@gmail.com>
2024-12-29 20:00:15 +0530

    Not wokring lol

--- Dynamic Programming/DPonSUbSequence/PrintAllLCS.cpp
+++ Dynamic Programming/DPonSUbSequence/PrintAllLCS.cpp
@@ -4,7 +4,8 @@ vector<string> all_longest_common_subsequences(string s, string t)
 {
     int ind1 = s.size();
     int ind2 = t.size();
-    vector<string> result;
+    set<string> result;
+    vector<string> finalString;
     vector<vector<int>> dp(ind1 + 1, vector<int>(ind2 + 1, 0));
     for (int i = 1; i <= ind1; i++)
     {
@@ -21,36 +22,66 @@ vector<string> all_longest_common_subsequences(string s, string t)
             }
         }
     }
+    cout << "DP Table:" << endl;
+    for (int i = 0; i <= ind1; i++) {
+        for (int j = 0; j <= ind2; j++) {
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;
+    }
     int len = dp[ind1][ind2];
-    for (int temp = 1; temp <= ind2; temp++)
+    for (int tempi = 1; tempi <= ind1; tempi++)
     {
-        if (dp[ind1][temp] == len)
+        for (int temp = 1; temp <= ind2; temp++)
         {
-            int i = ind1;
-            int j = temp;
-            string ans(len, '$');
-            int index = len - 1;
-            while (i > 0 && j > 0)
+            if (dp[tempi][temp] == len)
             {
-                if (s[i - 1] == t[j - 1])
+                int i = tempi;
+                int j = temp;
+                string ans(len, '$');
+                int index = len - 1;
+                while (i > 0 && j > 0)
                 {
-                    ans[len] = s[i - 1];
+                    if (s[i - 1] == t[j - 1])
+                    {
+                        ans[index] = s[i - 1];
+                        i--;
+                        j--;
+                        index--;
+                    }
+                    else if (dp[i][j - 1] > dp[i - 1][j])
+                    {
+                        j--;
+                    }
+                    else
+                        i--;
                 }
-                else if (dp[i][j - 1] > dp[i - 1][j])
-                {
-                    j--;
-                }
-                else
-                    i--;
+
+                result.insert(ans);
+                cout << ans << "|";
             }
-            result.push_back(ans);
         }
     }
+    for (auto i : result)
+    {
+        // cout << i << endl;
 
-    return result;
+        finalString.push_back(i);
+    }
+
+    return finalString;
 }
+
 int main()
 {
+    string text1 = "abaaa";
+    string text2 = "baabaca";
+
+    vector<string> lcs = all_longest_common_subsequences(text1, text2);
+    for (const string &str : lcs)
+    {
+        cout << str << endl;
+    }
 
     return 0;
 }
