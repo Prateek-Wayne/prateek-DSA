20a4f4dc0734c781641a0d05bd25ad69626e3d5e
Prateek-Wayne <lordprateekverma@gmail.com>
2025-01-01 22:45:51 +0530

    first iteration

---
+++ recursion/recursiveNumber.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+void helper(long long index, int n, long long first, long long &ans)
+{
+    if (index == n)
+        return;
+    long long temp = 1;
+    long long i = first;
+    for (; i < first + index; i++)
+    {
+        // temp = temp * i;
+        temp = (temp * i) % MOD;
+    }
+
+    ans += temp;
+    first = i;
+    helper(index + 1, n, i, ans);
+}
+
+long long sequence(int n)
+{
+    long long ans = 0;
+    helper(1, n + 1, 1, ans);
+    return ans % MOD;
+}
+int main()
+{
+    cout << sequence(11);
+    return 0;
+}
