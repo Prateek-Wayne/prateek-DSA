52883bbec046c089eac807a2bd7022ee6e6bbdd2
Prateek-Wayne <lordprateekverma@gmail.com>
2025-01-04 09:34:00 +0530

    ds

--- recursion/subsetSum.cpp
+++ recursion/subsetSum.cpp
@@ -1,28 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void helper(vector<int> &arr, int ind, vector<int> ds)
+void helper(vector<int> arr, int ind, vector<int> ds, vector<int> &ans)
 {
-    if (ind == 0)
+    if (ind < 0)
     {
-        ds.push_back(arr[0]);
         int sum = 0;
         for (auto i : ds)
             sum += i;
-        cout << sum << endl;
+        ans.push_back(sum);
+        // cout << sum << endl;
         return;
     }
-    // pick..
     ds.push_back(arr[ind]);
-    helper(arr, ind - 1, ds);
+    helper(arr, ind - 1, ds, ans);
     ds.pop_back();
-    helper(arr, ind - 1, ds);
+    helper(arr, ind - 1, ds, ans);
 }
-
+vector<int> subsetSums(vector<int> &arr)
+{
+    vector<int> ans;
+    helper(arr, arr.size() - 1, {}, ans);
+    return ans;
+}
 
 int main()
 {
-    vector<int> arr = {2, 3};
-    helper(arr, arr.size() - 1, {});
+    vector<int> arr = {1,2,2};
+    vector<int> ans;
+    ans = subsetSums(arr);
+    
     return 0;
 }
