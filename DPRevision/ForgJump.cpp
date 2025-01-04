a90e60d65880d2ae13d4cb0154ef9bfa3b1442ce
Prateek-Wayne <lordprateekverma@gmail.com>
2025-01-04 16:51:21 +0530

    qq

--- DPRevision/ForgJump.cpp
+++ DPRevision/ForgJump.cpp
@@ -1,21 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int helper(vector<int> &height, int ind)
+int helper(vector<int> &height, int ind, vector<int> &dp)
 {
     if (ind == 0)
         return 0;
-    int left = helper(height, ind - 1) + abs(height[ind] - height[ind - 1]);
+    if (dp[ind] != -1)
+        return dp[ind];
+    int left = helper(height, ind - 1, dp) + abs(height[ind] - height[ind - 1]);
     int right = INT_MAX;
     if (ind > 1)
-        right = helper(height, ind - 2) + abs(height[ind] - height[ind - 2]);
-    return min(left, right);
+        right = helper(height, ind - 2, dp) + abs(height[ind] - height[ind - 2]);
+    return dp[ind] = min(left, right);
 }
 
 int minimumEnergy(vector<int> &height, int n)
 {
     // Code here
-    return helper(height, n - 1);
+    vector<int> dp(n, -1);
+    return helper(height, n - 1, dp);
 }
 int main()
 {
