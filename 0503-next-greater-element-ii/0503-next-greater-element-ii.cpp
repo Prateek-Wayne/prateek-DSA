74a4585bb3ab26b4ca3ad8a90d586d2a1f78d36e
Prateek Verma <lordprateekverma@gmail.com>
2025-01-21 22:22:09 +0530

    Time: 0 ms (100%), Space: 28.5 MB (59.13%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -3,10 +3,6 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         stack<int> st;
     int n = nums.size();
-    // vector<int> arr=nums;
-    // for(int i=0;i<n;i++)
-    //     arr.push_back(nums[i]);
-    
     vector<int> ans(n, -1);
     for (int i = 2*n - 1; i >= 0; i--)
     {
@@ -18,8 +14,6 @@ public:
             ans[i%n] = st.top();
         st.push(nums[i%n]);
     }
-    // vector<int> ans2(ans.begin(),ans.begin()+n);
-    // return ans2;
     return ans;
     }
 };
