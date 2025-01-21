993aabf45b60e2f453729bd827c43ef527c08c0d
Prateek Verma <lordprateekverma@gmail.com>
2025-01-21 21:31:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.cpp
+++ Difficulty: Medium/Next Greater Element/next-greater-element.cpp
@@ -7,25 +7,25 @@ using namespace std;
 class Solution {
   public:
     // Function to find the next greater element for each element of the array.
-
-vector<int> nextLargerElement(vector<int> &arr)
-{
-    // code here
-   stack<int> st;
-    int n = arr.size();
-    vector<int> ans(n, -1);
-    for (int i = n - 1; i >= 0; i--)
-    {
-        while (!st.empty() &&st.top() <= arr[i])
-            st.pop();
-        if (st.empty())
-            ans[i] = -1;
-        else
-            ans[i] = st.top();
-        st.push(arr[i]);
+    vector<int> nextLargerElement(vector<int>& arr) {
+        // code here
+        stack<int> st;
+        int n=arr.size();
+        vector<int> ans(n,-1);
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.empty() && st.top()<=arr[i])
+                st.pop();
+            if(st.empty())
+                ans[i]=-1;
+            else
+            {
+                ans[i]=st.top();
+            }
+            st.push(arr[i]);
+        }
+        return ans;
     }
-    return ans;
-}
 };
 
 //{ Driver Code Starts.
