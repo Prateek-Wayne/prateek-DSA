2ed07f6fa50ac0b749d81272bd9b688ea12be66d
Prateek Verma <lordprateekverma@gmail.com>
2025-02-15 18:36:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int countFreq(vector<int>& nums, int target) {
+        // code here
+            int lb = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+    int ub = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
+    if (lb == nums.size() || nums[lb] != target)
+        return 0;
+    return ub-lb;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        int ans = ob.countFreq(arr, d);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
