6ffcaa78c4cafb578d84b73a9055a7c9b076a53f
Prateek Verma <lordprateekverma@gmail.com>
2025-02-28 21:51:46 +0530

    Time: 0 ms (100%), Space: 10.1 MB (5.59%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
+    long long helper(string s, long long res, long long ind, bool positive) {
+        if (ind == s.length() || ((s[ind] - '0') > 9 || (s[ind] - '0' < 0)))
+            return res;
+        long long num = s[ind] - '0';
+
+        res = res * 10 + num;
+        if (res > INT_MAX && !positive)
+            return INT_MIN;
+        if (res > INT_MAX && positive)
+            return INT_MAX;
+
+        return helper(s, res, ind + 1, positive);
+    }
+
     int myAtoi(string s) {
-        string resultant = "";
-        int trailingSpaces = 0;
         while (s[0] == ' ')
             s.erase(0, 1);
+        int res = 0;
         bool positive = true;
-        long long ans = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '-' && i == 0) {
+        if (s[0] == '-' || s[0] == '+') {
+            if (s[0] == '-') {
                 positive = false;
-                continue;
-            }
-            if (s[i] == '+' && i == 0) {
+                s.erase(0, 1);
+            } else if (s[0] == '+') {
                 positive = true;
-                continue;
+                s.erase(0, 1);
             }
-            int number = s[i] - '0';
-            if (number >= 0 && number <= 9) {
-                ans = ans * 10 + number;
-                if (ans > INT_MAX && !positive)
-                    return INT_MIN;
-                else if (ans > INT_MAX && positive)
-                    return INT_MAX;
-            } else
-                break;
         }
-        return positive ? ans : (-1 * ans);
+        long long ans = helper(s, 0, 0, positive);
+        if (ans != INT_MIN && !positive)
+            ans = ans * (-1);
+
+        // return ans % int(1e9 + 7);
+        return ans;
     }
 };
