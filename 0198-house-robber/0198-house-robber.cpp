fa1e35ce34b051d274b1289f1540b38ce87abe2e
Prateek Verma <lordprateekverma@gmail.com>
2025-03-17 10:08:08 +0530

    Time: 0 ms (100%), Space: 10.6 MB (44.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
+    int helper(vector<int>& nums, int n, vector<int>& dp) {
+        if (n == 0)
+            return nums[0];
+        if (n < 0)
+            return 0;
+        if (dp[n] != -1)
+            return dp[n];
+        int left = helper(nums, n - 1, dp) + 0;
+        int right = helper(nums, n - 2, dp) + nums[n];
+        return dp[n] = max(left, right);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
 
-int helper(vector<int> &arr, int ind, vector<int> &dp)
-{
-    if (ind == 0)
-        return arr[ind];
-      if (ind < 0)
-        return 0;
-    if (dp[ind] != -1)
-        return dp[ind];
-    int notRob = helper(arr, ind - 1, dp) + 0;
-    // int rob = INT_MIN;
-    // if (ind > 1)
-      int  rob = helper(arr, ind - 2, dp) + arr[ind];
-    return dp[ind] = max(rob, notRob);
-}
-
-int rob(vector<int> &nums)
-{
-    int n = nums.size() - 1;
-    vector<int> dp(n + 1, -1);
-    return helper(nums, n, dp);
-}
+        return helper(nums, n - 1, dp);
+    }
 };
