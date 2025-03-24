74fb5cb5fe2d0d02df3af97d598b1a0084553575
Prateek Verma <lordprateekverma@gmail.com>
2025-03-24 13:08:47 +0530

    Time: 96 ms (16.54%), Space: 75.9 MB (61.2%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,50 +1,22 @@
 class Solution {
 public:
-    int helper(string& s1, string& s2, int ind1, int ind2,
-               vector<vector<int>>& dp) {
-        if (ind1 == 0 || ind2 == 0)
-            return 0;
-
-        if (dp[ind1][ind2] != -1)
-            return dp[ind1][ind2];
-
-        if (s1[ind1 - 1] == s2[ind2 - 1])
-            return dp[ind1][ind2] = 1 + helper(s1, s2, ind1 - 1, ind2 - 1, dp);
-        return dp[ind1][ind2] = max(helper(s1, s2, ind1 - 1, ind2, dp),
-                                    helper(s1, s2, ind1, ind2 - 1, dp));
-    }
-
-    // int longestPalindromeSubseq(string s) {
-    //     int ind1 = s.length();
-    //     int ind2 = ind1;
-    //     string s2 = s;
-    //     vector<vector<int>> dp(ind1 + 1, vector<int>(ind2 + 1, -1));
-    //     reverse(s2.begin(), s2.end());
-    //     return helper(s, s2, ind1, ind2, dp);
-    // }
-    int longestPalindromeSubseq(string s)
-{
-    int ind1 = s.length();
-    int ind2 = ind1;
-    string s2 = s;
-    reverse(s2.begin(), s2.end());
-    vector<vector<int>> dp(ind1 + 1, vector<int>(ind2 + 1, -1));
-    // bases
-    for (int i = 0; i <= ind1; i++)
-        dp[i][0] = 0;
-    for (int j = 0; j <= ind2; j++)
-        dp[0][j] = 0;
-    // iterations..
-    for (int i = 1; i <= ind1; i++)
-    {
-        for (int j = 1; j <= ind2; j++)
-        {
-            if (s[i - 1] == s2[j - 1])
-                dp[i][j] = 1 + dp[i - 1][j - 1];
-            else
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+    int longestCommonSubsequence(string text1, string text2) {
+        int n1 = text1.size(), n2 = text2.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (text1[i - 1] == text2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
         }
+
+        return dp[n1][n2];
     }
-    return dp[ind1][ind2];
-}
+    int longestPalindromeSubseq(string s) {
+        string s2=s;
+        reverse(s.begin(),s.end());
+        return longestCommonSubsequence(s,s2);
+    }
 };
