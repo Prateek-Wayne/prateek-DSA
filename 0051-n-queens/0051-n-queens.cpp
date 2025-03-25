f97a3257dd0002532fc6bfe828e2d471d28bd168
Prateek Verma <lordprateekverma@gmail.com>
2025-03-25 21:08:45 +0530

    Time: 2 ms (74.6%), Space: 10.3 MB (55.93%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,52 +1,62 @@
 class Solution {
 public:
-    vector<vector<string>> solveNQueens(int n) {
-        vector<string> board(n, string(n, '.'));
-        vector<vector<string>> ans;
-        helper(board, ans, 0);
-        return ans;
-    }
-
-    void helper(vector<string>& board, vector<vector<string>>& ans, int col) {
-        int n = board.size();
-        if (col == n) {
-            ans.push_back(board);
-            return;
-        }
-        //
-        for (int row = 0; row < n; row++) {
-            if (isSafe(board, row, col)) {
-                board[row][col] = 'Q';
-                helper(board, ans, col + 1);
-                board[row][col] = '.';
-            }
-        }
+vector<vector<string>> solveNQueens(int n)
+{
+    vector<string> board(n, string(n, '.'));
+    vector<vector<string>> ans;
+    helper(board, 0, ans);
+    return ans;
+}
+void helper(vector<string> &board, int col, vector<vector<string>> &ans)
+{
+    int n = board.size();
+    // base case..
+    if (col == n)
+    {
+        ans.push_back(board);
         return;
     }
 
-    bool isSafe(vector<string>& board, int row, int col) {
-        int tempRow = row, tempCol = col, n = board.size();
-        while (tempRow >= 0 && tempCol >= 0) {
-            if (board[tempRow][tempCol] == 'Q')
-                return false;
-            tempRow--;
-            tempCol--;
+    for (int i = 0; i < n; i++)
+    {
+
+        if (issafe(board, i, col))
+        {
+            board[i][col] = 'Q';
+            helper(board, col + 1, ans);
+            board[i][col] = '.';
         }
-        tempRow = row;
-        tempCol = col;
-        while (tempCol >= 0) {
-            if (board[tempRow][tempCol] == 'Q')
-                return false;
-            tempCol--;
-        }
-        tempRow = row;
-        tempCol = col;
-        while (tempRow < n && tempCol >= 0) {
-            if (board[tempRow][tempCol] == 'Q')
-                return false;
-            tempRow++;
-            tempCol--;
-        }
-        return true;
     }
+}
+
+bool issafe(vector<string> &board, int row, int col)
+{
+    int n = board.size();
+    int tempRow = row;
+    int tempCol = col;
+    while (tempRow >= 0 && tempCol >= 0)
+    {
+        if (board[tempRow][tempCol] == 'Q')
+            return false;
+        tempRow--;
+        tempCol--;
+    }
+    tempRow = row, tempCol = col;
+    while (tempRow < n && tempCol >= 0)
+    {
+        if (board[tempRow][tempCol] == 'Q')
+            return false;
+        tempRow++;
+        tempCol--;
+    }
+    tempRow = row;
+    tempCol = col;
+    while (tempCol >= 0)
+    {
+        if (board[tempRow][tempCol] == 'Q')
+            return false;
+        tempCol--;
+    }
+    return true;
+}
 };
