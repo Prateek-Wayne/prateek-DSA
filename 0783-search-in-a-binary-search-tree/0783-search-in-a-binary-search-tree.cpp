ee602c7872b2ee39f2fc5dcdbf7f97e93fcadafe
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2025-04-20 17:49:31 +0530

    Time: 0 ms (100%), Space: 35.5 MB (61.71%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
@@ -11,15 +11,16 @@
  */
 class Solution {
 public:
-TreeNode *searchBST(TreeNode *root, int val)
-{
-    if (!root)
-        return NULL;
-    if (val == root->val)
-        return root;
-    if (val > root->val)
-        return searchBST(root->right, val);
-    else
-        return searchBST(root->left, val);
-}
+
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(!root)
+            return NULL;
+        if(root->val==val)
+            return root;
+        if(root->val>val){
+            return searchBST(root->left,val);
+        }
+        else
+            return searchBST(root->right,val);
+    }
 };
