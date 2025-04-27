00062bc0025a0b4ef4412bbf4c54a209c70fa01a
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2025-04-27 11:03:11 +0530

    Time: 8 ms (26.61%), Space: 117.7 MB (16.62%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr) {
-        long long modd=1e9+7;
-        long long odd=0 ,even=1 ,ans=0 ,sum=0;
-        for(int i=0;i<arr.size();i++)
-        {
-            sum+=arr[i];
-            if(sum &1)
-            {
+        int mod=1e9+7;
+        int even=1;
+        int odd=0;
+        int n=arr.size();
+        vector<int> prefixSum(n,0);
+        prefixSum[0]=arr[0];
+        long long ans=0;
+        for(int i=1;i<n;i++){
+            prefixSum[i]=prefixSum[i-1]+arr[i];
+        }
+        for(int i=0;i<n;i++){
+            if(prefixSum[i]&1){
                 ans+=even;
+                ans=ans%mod;
                 odd++;
             }
-            else
-            {
+            else{
                 ans+=odd;
+                ans=ans%mod;
                 even++;
             }
         }
-        return ans%modd;
+        return ans;
     }
 };
