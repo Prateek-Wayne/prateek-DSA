42473b530548a86e99d00c3893f1e8e84713e454
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2025-05-19 19:10:44 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/nCr/n-cr.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int nCr(int n, int r) {
+        long long ans=1;
+        for(int i=0;i<r;i++){
+            ans=ans*(n-i);
+            ans=ans/(i+1);
+        }
+        return int(ans);
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, r;
+        cin >> n >> r;
+
+        Solution ob;
+        cout << ob.nCr(n, r) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
