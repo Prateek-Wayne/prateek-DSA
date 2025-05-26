ed1619a8da91fdd6f802b5e2d757444cfe7b3f04
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2025-05-26 11:57:10 +0530

    Time: 0 ms (100%), Space: 17.9 MB (89.38%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-
-bool search(vector<int> &nums, int target)
-{
-    int n = nums.size();
-    int low = 0, high = n - 1;
-    while (low <= high)
-    {
-        int mid = low + (high - low) / 2;
-        if (nums[mid] == target)
-            return true;
-        if (nums[low] == nums[mid] && nums[mid] == nums[high])
-        {
-            low = low + 1;
-            high = high - 1;
-            continue;
-        }
-        if (nums[low] <= nums[mid])
-        {
-            if (nums[low] <= target && target <= nums[mid])
-                high = mid - 1;
+    bool search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target)
+                return true;
+            // specific...
+           if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low=low+1;
+                high=high-1;
+                continue;
+            }
+            // search only in sorted part...
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && nums[mid]>=target)
+                {
+                    high=mid-1;
+                }
+                else
+                    low=mid+1;
+            }
             else
-                low = mid + 1;
+            {
+                // right is sorted...
+                if(nums[mid]<=target && nums[high]>=target){
+                    low=mid+1;
+                }
+                else
+                    high=mid-1;
+            }
         }
-        else
-        {
-            if (nums[mid] <= target && target <= nums[high])
-                low = mid + 1;
-            else
-                high = mid - 1;
-        }
+        return false;
     }
-    return false;
-}
 };
