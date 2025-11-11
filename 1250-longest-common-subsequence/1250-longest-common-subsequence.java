2945c598731b0738b48f84e59458c30067efae5a
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2025-11-11 21:53:50 +0530

    Time: 39 ms (5.43%), Space: 75.2 MB (5.04%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,11 +1,11 @@
 class Solution {
-   int helper(String text1, String text2, int index1, int index2, Integer[][] dp) {
-        if (index1 < 0 || index2 < 0)
+    int helper(String text1, String text2, int index1, int index2, Integer[][] dp) {
+        if (index1 == 0 || index2 == 0)
             return 0;
         if (dp[index1][index2] != null)
             return dp[index1][index2];
 
-        if (text1.charAt(index1) == text2.charAt(index2))
+        if (text1.charAt(index1-1) == text2.charAt(index2-1))
             return dp[index1][index2] = 1 + helper(text1, text2, index1 - 1, index2 - 1, dp);
         else {
             return dp[index1][index2] = 0 + Math.max(helper(text1, text2, index1 - 1, index2, dp),
@@ -16,7 +16,23 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int index1 = text1.length();
         int index2 = text2.length();
-        Integer[][] dp = new Integer[index1][index2];
-        return helper(text1, text2, index1 - 1, index2 - 1, dp);
+        Integer[][] dp = new Integer[index1 + 1][index2 + 1];
+        for (int i = 0; i <= index1; i++)
+            dp[i][0] = 0;
+        for (int i = 0; i <= index2; i++)
+            dp[0][i] = 0;
+        for (int i = 1; i <= index1; i++) {
+            for (int j = 1; j <= index2; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1))
+                     dp[i][j] = 1 + dp[i - 1][j - 1]; // helper(text1, text2, index1 - 1, index2 - 1, dp);
+                else {
+                     dp[i][j] = 0 + Math.max(dp[i - 1][j], dp[i][j - 1]); // Math.max(helper(text1, text2, index1
+                                                                                // - 1, index2, dp), helper(text1,
+                                                                                // text2, index1, index2 - 1, dp));
+                }
+            }
+        }
+
+        return dp[index1][index2];
     }
 }
