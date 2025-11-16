6cf8d30db059c60d567ba3b064a66d6cfd6f3909
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2025-11-16 16:07:48 +0530

    Time: 5 ms (56.84%), Space: 44.1 MB (63.68%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
+                if (s.length() != t.length())
             return false;
-        HashMap<Character, Integer> mp = new HashMap<>();
-        HashMap<Character, Integer> mp1 = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            mp.put(s.charAt(i), mp.getOrDefault(s.charAt(i), 0) + 1);
-            mp1.put(t.charAt(i), mp1.getOrDefault(t.charAt(i), 0) + 1);
-
+        int[] p = new int[26];
+        int[] q=new int[26];
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            int c1 = s.charAt(i) - 'a';
+            int c2 = t.charAt(i) - 'a';
+            p[c1] = p[c1] + 1;
+            q[c2] = q[c2] + 1;
         }
-        return mp.equals(mp1);
+       for (int i = 0; i < p.length; i++) {
+            if (p[i]!=q[i])
+                return false;
+        }
+        return true;
     }
 }
