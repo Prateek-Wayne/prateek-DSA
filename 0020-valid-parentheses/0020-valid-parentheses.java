ec393403c585273cd8125c9c19051c34b006c1f9
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-02-02 07:48:46 +0530

    Time: 4 ms (42.89%), Space: 43.5 MB (23.87%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,18 +1,23 @@
 class Solution {
-    public boolean isValid(String s) {
+public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
-        if (s.isEmpty())
-            return true;
-        st.add(s.charAt(0));
-        for (int i = 1; i < s.length(); i++) {
-            if(st.isEmpty())
-                st.add(s.charAt(i));
-            else if (s.charAt(i) == ')' && st.peek() == '(' ||
-                    s.charAt(i) == '}' && st.peek() == '{' ||
-                    s.charAt(i) == ']' && st.peek() == '[')
-                st.pop();
-            else
-                st.add(s.charAt(i));
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            if (c == '{' || c == '[' || c == '(')
+                st.add(c);
+            else {
+                if (st.isEmpty())
+                    return false;
+                Character top = st.peek();
+                if (top == '(' && c == ')')
+                    st.pop();
+                else if (top == '{' && c == '}')
+                    st.pop();
+                else if (top == '[' && c == ']')
+                    st.pop();
+                else
+                    st.add(c);
+            }
         }
         return st.isEmpty();
 
