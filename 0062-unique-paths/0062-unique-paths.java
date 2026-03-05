97166d28f64c076ab83787f229f769608946757b
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-03-05 19:48:52 +0530

    Time: 1 ms (14.35%), Space: 42.3 MB (46.58%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -24,14 +24,39 @@ class Solution {
         return dp[m][n] = left + right;
     }
 
+    // public int uniquePaths(int m, int n) {
+    //     int[][] dp = new int[m + 1][n + 1];
+    //     for (int i = 0; i <= m; i++) {
+    //         int[] temp = new int[n + 1];
+    //         Arrays.fill(temp, -1);
+    //         dp[i] = temp;
+    //     }
+
+    //     return helper(m - 1, n - 1, dp);
+    // }
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i <= m; i++) {
-            int[] temp = new int[n + 1];
-            Arrays.fill(temp, -1);
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            int[] temp = new int[n];
             dp[i] = temp;
         }
+        // first column
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
 
-        return helper(m - 1, n - 1, dp);
+        // first row
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                int left = dp[i][j - 1];
+                int right = dp[i - 1][j];
+                dp[i][j] = left + right;
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 }
