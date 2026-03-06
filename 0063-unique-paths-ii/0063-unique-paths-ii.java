2f881c62e55050c8019eec375fabcf3eb03bbee4
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-03-06 08:02:01 +0530

    Time: 0 ms (100%), Space: 43.6 MB (63.95%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,40 +1,30 @@
 class Solution {
-    int helper(int[][] obstacleGrid, int m, int n, int[][] dp) {
-        if (m == 0 && n == 0) {
-            if (obstacleGrid[m][n] != 1)
-                return 1;
-            return 0;
-        }
-        if (dp[m][n] != -1)
-            return dp[m][n];
-        // left...
-        int left = 0;
-        if (n > 0) {
-            if (obstacleGrid[m][n - 1] != 1) {
-                left = helper(obstacleGrid, m, n - 1, dp);
-            }
-        }
-        // right...
-        int right = 0;
-        if (m > 0) {
-            if (obstacleGrid[m - 1][n] != 1) {
-                right = helper(obstacleGrid, m - 1, n, dp);
-            }
-        }
-        return dp[m][n] = left + right;
-    }
 
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        if (obstacleGrid[m - 1][n - 1] == 1)
-            return 0;
+
         int[][] dp = new int[m][n];
         for (int i = 0; i < m; i++) {
-            int[] temp = new int[n];
-            Arrays.fill(temp, -1);
-            dp[i] = temp;
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                } else if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                int left = 0;
+                if (j > 0) {
+                    left = dp[i][j - 1];
+                }
+                int right = 0;
+                if (i > 0) {
+                    right = dp[i - 1][j];
+                }
+                dp[i][j] = left + right;
+            }
         }
-        return helper(obstacleGrid, m - 1, n - 1, dp);
+        return dp[m - 1][n - 1];
     }
 }
