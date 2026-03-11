5850aee33dfc7a48c215fa7f6e2129642d9c8287
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-03-11 21:43:20 +0530

    Time: 49 ms (6.61%), Space: 47.4 MB (13.88%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,26 +1,41 @@
 class Solution {
-    int helper(int[] coins, int amount, int index, Integer[][] dp) {
-        if (index == 0) {
-            if (amount % coins[index] == 0)
-                return amount / coins[index];
-            return (int) 1e9;
-        }
-        if (dp[index][amount] != null)
-            return dp[index][amount];
+int helper(int[] coins, int amount, int index, int[][] dp) {
 
-        // not pick
-        int notPick = helper(coins, amount, index - 1, dp);
-        // pick
-        int pick = Integer.MAX_VALUE;
-        if (coins[index] <= amount)
-            pick = 1 + helper(coins, amount - coins[index], index, dp);
-        return dp[index][amount] = Math.min(notPick, pick);
+    if (index == 0) {
+        if (amount % coins[0] == 0)
+            return amount / coins[0];
+        return Integer.MAX_VALUE;
     }
 
-    public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        Integer[][] dp = new Integer[n][amount + 1];
-        int ans = helper(coins, amount, n - 1, dp);
-        return ans >= 1e9 ? -1 : ans;
+    if (dp[amount][index] != -1)
+        return dp[amount][index];
+
+    int notPick = helper(coins, amount, index - 1, dp);
+
+    int pick = Integer.MAX_VALUE;
+
+    if (amount >= coins[index]) {
+
+        int res = helper(coins, amount - coins[index], index, dp);
+
+        if (res != Integer.MAX_VALUE)
+            pick = 1 + res;
     }
+
+    return dp[amount][index] = Math.min(pick, notPick);
+}
+
+public int coinChange(int[] coins, int amount) {
+
+    int n = coins.length;
+
+    int[][] dp = new int[amount + 1][n];
+
+    for (int i = 0; i <= amount; i++)
+        Arrays.fill(dp[i], -1);
+
+    int ans = helper(coins, amount, n - 1, dp);
+
+    return ans == Integer.MAX_VALUE ? -1 : ans;
+}
 }
