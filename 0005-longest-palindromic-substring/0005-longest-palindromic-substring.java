6de1fb4a9c574ef363ccc0c5698abf4b5e430431
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-03-17 22:46:41 +0530

    Time: 390 ms (15.73%), Space: 272.6 MB (5.03%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,9 +1,14 @@
 class Solution {
-    Boolean solve(String s, int i, int j) {
+     Boolean solve(String s, int i, int j, int[][] dp) {
         if (i >= j)
             return true;
+        if (dp[i][j] != -1)
+            return dp[i][j] == 0 ? false : true;
         if (s.charAt(i) == s.charAt(j)) {
-            return solve(s, i+1, j-1);
+
+            Boolean ans = solve(s, i+1, j-1, dp);
+            dp[i][j] = ans == true ? 1 : 0;
+            return ans;
         }
         return false;
     }
@@ -11,10 +16,16 @@ class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
         StringBuilder ans = new StringBuilder();
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = 0; i <= n; i++) {
+            int[] temp = new int[n + 1];
+            Arrays.fill(temp, -1);
+            dp[i] = temp;
+        }
         int maxLength = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (solve(s, i, j)) {
+                if (solve(s, i, j, dp)) {
                     if (maxLength < j - i + 1) {
                         maxLength = j - i + 1;
                         ans.delete(0, ans.length());
