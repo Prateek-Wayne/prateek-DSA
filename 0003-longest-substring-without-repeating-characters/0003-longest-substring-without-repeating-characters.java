e242ec87ff75bf1dee7c88bd0cb81189e5cd5f9a
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-03-21 15:18:44 +0530

    Time: 7 ms (37.27%), Space: 45.8 MB (67.76%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -6,19 +6,24 @@ class Solution {
         int n = s.length();
         int ans = 0;
         while (j < n) {
-            Character charJ = s.charAt(j);
-            mp.put(charJ, mp.getOrDefault(charJ, 0) + 1);
+            Character c = s.charAt(j);
+            mp.put(c, mp.getOrDefault(c, 0) + 1);
             if (j - i + 1 == mp.size()) {
-                ans = Math.max(ans, mp.size());
-            } else if (j - i + 1 > mp.size()) {
-                Character charI = s.charAt(i);
-                // remove for i...
-                mp.put(charI, mp.get(charI) - 1);
-                if (mp.get(charI) == 0)
-                    mp.remove(charI);
-                i++;
-                if (j - i + 1 == mp.size())
-                    ans = Math.max(ans, mp.size());
+                ans = Math.max(ans, j - i + 1);
+            } else {
+                // remove calculations..
+                while (j - i + 1 > mp.size()) {
+                    Character ith = s.charAt(i);
+                    mp.put(ith, mp.get(ith) - 1);
+                    if (mp.get(ith) == 0) {
+                        mp.remove(ith);
+                    }
+                    i++;
+                }
+                if (j - i + 1 == mp.size()) {
+                    ans = Math.max(ans, j - i + 1);
+                }
+
             }
             j++;
         }
