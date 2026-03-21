ff6b1a144fd64f40c7bfc32c04a77e1f8c737ca2
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-03-21 15:50:39 +0530

    Time: 20 ms (34.89%), Space: 46.2 MB (29.04%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,28 +1,33 @@
 class Solution {
-  public boolean checkInclusion(String p, String s) {
-        List<Integer> ans = new ArrayList<>();
-        HashMap<Character, Integer> mp = new HashMap<>();
-        for (int i = 0; i < p.length(); i++) {
-            mp.put(p.charAt(i), mp.getOrDefault(p.charAt(i), 0) + 1);
+ public boolean checkInclusion(String s1, String s2) {
+        HashMap<Character, Integer> mp1 = new HashMap<>();
+        HashMap<Character, Integer> mp2 = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+            Character c = s1.charAt(i);
+            mp1.put(c, mp1.getOrDefault(c, 0) + 1);
         }
+        int n1 = s1.length();
+        int n = s2.length();
         int i = 0;
         int j = 0;
-        int n = s.length();
-        int k = p.length();
-        HashMap<Character, Integer> slidingMap = new HashMap<>();
         while (j < n) {
-            Character charJ = s.charAt(j);
-            slidingMap.put(charJ, slidingMap.getOrDefault(charJ, 0) + 1);
-            if (j - i + 1 == k) {
-                Character charI = s.charAt(i);
-                if (mp.equals(slidingMap)) {
+            Character cj = s2.charAt(j);
+            mp2.put(cj, mp2.getOrDefault(cj, 0) + 1);
+            if (j - i + 1 == n1) {
+                if (mp1.equals(mp2))
                     return true;
+            } else {
+                while (j - i + 1 > n1) {
+                    Character ci = s2.charAt(i);
+                    mp2.put(ci, mp2.get(ci) - 1);
+                    if (mp2.get(ci) == 0)
+                        mp2.remove(ci);
+                    i++;
                 }
-                slidingMap.put(charI, slidingMap.get(charI) - 1);
-                if (slidingMap.get(charI) == 0) {
-                    slidingMap.remove(charI);
+                if (j - i + 1 == n1) {
+                    if (mp1.equals(mp2))
+                        return true;
                 }
-                i++;
             }
             j++;
         }
