596e8d43f0b8ac53bf7aa7ddaae128b59af2ff1e
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-04-05 08:24:03 +0530

    Time: 8 ms (24.19%), Space: 45.3 MB (95.91%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,19 +1,26 @@
 class Solution {
     public boolean judgeCircle(String moves) {
 
-        int countUp = 0;
-        int countRight = 0;
+        int x = 0;
+        int y = 0;
         for (int i = 0; i < moves.length(); i++) {
             Character c = moves.charAt(i);
-            if (c == 'U')
-                countUp++;
-            else if (c == 'D')
-                countUp--;
-            else if (c == 'R')
-                countRight++;
-            else if (c == 'L')
-                countRight--;
+            switch (c) {
+                case 'U':
+                    y++;
+                    break;
+                case 'D':
+                    y--;
+                    break;
+                case 'R':
+                    x++;
+                    break;
+                case 'L':
+                    x--;
+                    break;
+            }
         }
-        return countRight == 0 && countUp == 0;
+        return x == 0 && y == 0;
+
     }
 }
