78c698507bbc8c35a2b1372ab3521a9353f495e9
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-04-06 08:10:02 +0530

    Time: 41 ms (99.15%), Space: 130.2 MB (95.37%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,88 +1,75 @@
-
 class DLL {
-    int val1;
-    int val2;
-    DLL prev;
+    int key;
+    int val;
     DLL next;
+    DLL prev;
 
-    DLL(int val1, int val2) {
-        this.val1 = val1;
-        this.val2 = val2;
-        this.prev = null;
+    DLL(int key, int val) {
+        this.key = key;
+        this.val = val;
         this.next = null;
-    }
-
-    DLL() {
-        this.val1 = -1;
-        this.val2 = -1;
         this.prev = null;
-        this.next = null;
     }
 }
 
-public class LRUCache {
-    DLL head = new DLL();
-    DLL tail = new DLL();
-    int capacity;
+class LRUCache {
     HashMap<Integer, DLL> mp = new HashMap<>();
+    DLL head;
+    DLL tail;
+    int capacity;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
+        head = new DLL(-1, -1); // dummy head
+        tail = new DLL(-1, -1); // dummy tail
         head.next = tail;
         tail.prev = head;
     }
 
-    public void deleteNode(DLL node) {
+    void delete(DLL node) {
         DLL prevNode = node.prev;
         DLL nextNode = node.next;
-        prevNode.next = prevNode.next.next;
-        nextNode.prev = nextNode.prev.prev;
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
         node.prev = null;
         node.next = null;
-        return;
     }
 
-    public void addToHead(DLL curr) {
-        DLL nextToHead = head.next;
-        head.next = curr;
-        curr.prev = head;
-        curr.next = nextToHead;
-        nextToHead.prev = curr;
-        return;
+    void add(DLL node) {
+        DLL nextTohead = head.next;
+        node.next = nextTohead;
+        node.prev = head;
+        head.next = node;
+        nextTohead.prev = node;
     }
 
     public int get(int key) {
         if (mp.containsKey(key)) {
-            DLL existingNode = mp.get(key);
-            deleteNode(existingNode);
-            addToHead(existingNode);
-            return existingNode.val2;
-        } else {
-            return -1;
+            DLL d = mp.get(key);
+            delete(d);
+            add(d);
+            return d.val;
         }
+        return -1;
     }
 
     public void put(int key, int value) {
         if (mp.containsKey(key)) {
-            DLL curr = mp.get(key);
-            curr.val2 = value;
-            deleteNode(curr);
-            addToHead(curr);
-            return;
-        } else if (mp.size() < capacity) {
-            DLL newNode = new DLL(key, value);
-            addToHead(newNode);
-            mp.put(key, newNode);
-            return;
+            // Update value and move to front
+            DLL node = mp.get(key);
+            node.val = value;
+            delete(node);
+            add(node);
         } else {
-            // delete last node...
-            DLL prevToHead = tail.prev;
-            mp.remove(prevToHead.val1);
-            deleteNode(prevToHead);
+            if (mp.size() == capacity) {
+                // Remove least recently used
+                DLL leastUsedNode = tail.prev;
+                delete(leastUsedNode);
+                mp.remove(leastUsedNode.key);
+            }
             DLL newNode = new DLL(key, value);
+            add(newNode);
             mp.put(key, newNode);
-            addToHead(newNode);
-            return;
         }
     }
 }
