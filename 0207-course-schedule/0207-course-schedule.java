5b6d5387001158728875ae344d6543f15e650233
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-04-25 10:22:35 +0530

    Time: 7 ms (59.63%), Space: 46.8 MB (93.57%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,34 +1,34 @@
 class Solution {
-   boolean dfs(List<List<Integer>> adj, boolean[] vis, boolean[] path, int curr) {
-        vis[curr] = true;
-        path[curr] = true;
-        for (Integer i : adj.get(curr)) {
-            if (!vis[i]) {
-                if (dfs(adj, vis, path, i))
-                    return true;
-            } else if (path[i])
-                return true;
-        }
-        path[curr] = false;
-        return false;
-    }
-
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
+ public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] inDegree = new int[numCourses];
         List<List<Integer>> adj = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        int countOfNode = 0;
+
+        // ........................................................
         for (int i = 0; i < numCourses; i++) {
             adj.add(new ArrayList<>());
         }
         for (int[] arr : prerequisites) {
             adj.get(arr[1]).add(arr[0]);
+            inDegree[arr[0]]++;
         }
-        boolean[] vis = new boolean[numCourses];
-        boolean[] path = new boolean[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            if (!vis[i]) {
-                if (dfs(adj, vis, path, i))
-                    return false;
+            if (inDegree[i] == 0) {
+                q.add(i);
+                countOfNode++;
             }
         }
-        return true;
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            for (Integer v : adj.get(u)) {
+                inDegree[v]--;
+                if (inDegree[v] == 0) {
+                    q.add(v);
+                    countOfNode++;
+                }
+            }
+        }
+        return countOfNode == numCourses;
     }
 }
