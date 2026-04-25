79fa9f8b6059a9dd2be68f53b3895b9d31684f63
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-04-26 00:00:12 +0530

    Time: 220 ms (17.69%), Space: 85.9 MB (5.08%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,39 +1,26 @@
 class Solution {
-     Boolean solve(String s, int i, int j, int[][] dp) {
-        if (i >= j)
-            return true;
-        if (dp[i][j] != -1)
-            return dp[i][j] == 0 ? false : true;
-        if (s.charAt(i) == s.charAt(j)) {
-
-            Boolean ans = solve(s, i+1, j-1, dp);
-            dp[i][j] = ans == true ? 1 : 0;
-            return ans;
+   public String longestPalindrome(String s) {
+        int m = s.length();
+        String p = "";
+        for (int i = m - 1; i >= 0; i--) {
+            p += s.charAt(i);
         }
-        return false;
-    }
+        int[][] dp = new int[m + 1][m + 1];
 
-    public String longestPalindrome(String s) {
-        int n = s.length();
-        StringBuilder ans = new StringBuilder();
-        int[][] dp = new int[n + 1][n + 1];
-        for (int i = 0; i <= n; i++) {
-            int[] temp = new int[n + 1];
-            Arrays.fill(temp, -1);
-            dp[i] = temp;
-        }
-        int maxLength = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (solve(s, i, j, dp)) {
-                    if (maxLength < j - i + 1) {
-                        maxLength = j - i + 1;
-                        ans.delete(0, ans.length());
-                        ans = new StringBuilder(s.subSequence(i, j + 1));
+        int maxLen = 0, endIdx = 0;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    // validity check: ensures it's actually a palindrome
+                    if (dp[i][j] > maxLen && i + j == m + dp[i][j]) {
+                        maxLen = dp[i][j];
+                        endIdx = i;
                     }
                 }
             }
         }
-        return ans.toString();
+        return s.substring(endIdx - maxLen, endIdx);
+
     }
 }
