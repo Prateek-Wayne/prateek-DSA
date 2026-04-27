c26493cfacc00a72535fa98a8ff65fa67fd369b8
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-04-27 19:12:56 +0530

    Time: 0 ms (100%), Space: 42.3 MB (29.19%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,62 +1,21 @@
 class Solution {
- boolean isValid(int m, int n) {
-        if (m >= 0 && n >= 0)
-            return true;
-        return false;
-    }
-
-    int helper(int m, int n, int[][] dp) {
-        // base conditon.
-        if (m == 0 && n == 0) {
+    int helper(int m,int n,int[][] dp){
+        if(m<0 || n<0 )
+            return 0;
+        if(m==0 && n==0)
             return 1;
-        }
-        if (dp[m][n] != -1) {
+        if(dp[m][n]!=-1)
             return dp[m][n];
-        }
-        int left = 0;
-        if (isValid(m, n - 1)) {
-            left = helper(m, n - 1, dp);
-        }
-        int right = 0;
-        if (isValid(m - 1, n)) {
-            right = helper(m - 1, n, dp);
-        }
-        return dp[m][n] = left + right;
+        
+        int top=helper(m-1,n,dp);
+        int down=helper(m,n-1,dp);
+        return dp[m][n]=top+down;
     }
-
-    // public int uniquePaths(int m, int n) {
-    //     int[][] dp = new int[m + 1][n + 1];
-    //     for (int i = 0; i <= m; i++) {
-    //         int[] temp = new int[n + 1];
-    //         Arrays.fill(temp, -1);
-    //         dp[i] = temp;
-    //     }
-
-    //     return helper(m - 1, n - 1, dp);
-    // }
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            int[] temp = new int[n];
-            dp[i] = temp;
+        int[][] dp=new int[m+1][n+1];
+        for(int[] temp:dp){
+            Arrays.fill(temp,-1);
         }
-        // first column
-        for (int i = 0; i < m; i++) {
-            dp[i][0] = 1;
-        }
-
-        // first row
-        for (int j = 0; j < n; j++) {
-            dp[0][j] = 1;
-        }
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                int left = dp[i][j - 1];
-                int right = dp[i - 1][j];
-                dp[i][j] = left + right;
-            }
-        }
-        return dp[m - 1][n - 1];
+        return helper(m-1,n-1,dp);
     }
 }
