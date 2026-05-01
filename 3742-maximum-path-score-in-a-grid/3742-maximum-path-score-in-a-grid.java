2789850a0e4c2c7895afcff371686f361922cfaf
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-05-02 00:33:57 +0530

    Time: 335 ms (51.61%), Space: 116.3 MB (24.19%) - LeetHub

--- 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.java
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.java
@@ -1,64 +1,44 @@
 class Solution {
+ int valueToSubtract(int[][] grid, int i, int j) {
+        if (grid[i][j] > 0) {
+            return 1;
+        }
+        return 0;
+    }
+
+    int helper(int[][] grid, int i, int j, int k, int[][][] dp) {
+        int m = grid.length;
+        int n = grid[0].length;
+        if (k < 0)
+            return Integer.MIN_VALUE;
+        if (i < 0 || j < 0 || i >= m || j >= n)
+            return Integer.MIN_VALUE;
+        if (i == 0 && j == 0) {
+            return (k >= 0) ? 0 : Integer.MIN_VALUE;
+        }
+        if (dp[i][j][k] != -1)
+            return dp[i][j][k];
+
+        int top = helper(grid, i - 1, j, k - valueToSubtract(grid, i, j), dp);
+        int left = helper(grid, i, j - 1, k - valueToSubtract(grid, i, j), dp);
+        int best = Math.max(top, left);
+        if (best == Integer.MIN_VALUE)
+            return dp[i][j][k] = best;
+        return dp[i][j][k] = grid[i][j] + Math.max(top, left);
+    }
+
     public int maxPathScore(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-
-        int[][][] dp = new int[m][n][k + 1];
-
-        // initialize with -1
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int c = 0; c <= k; c++) {
-                    dp[i][j][c] = -1;
-                }
+        int[][][] dp = new int[m + 1][n + 1][k + 1];
+        for (int[][] temp : dp) {
+            for (int[] temp2 : temp) {
+                Arrays.fill(temp2, -1);
             }
+            // Arrays.fill(temp, -1);
         }
-
-        dp[0][0][0] = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
-                for (int c = 0; c <= k; c++) {
-
-                    if (dp[i][j][c] == -1) continue;
-
-                    // move down
-                    if (i + 1 < m) {
-                        int val = grid[i + 1][j];
-                        int cost = (val == 0 ? 0 : 1);
-
-                        int nc = c + cost;
-                        if (nc <= k) {
-                            dp[i + 1][j][nc] = Math.max(
-                                dp[i + 1][j][nc],
-                                dp[i][j][c] + val
-                            );
-                        }
-                    }
-
-                    // move right
-                    if (j + 1 < n) {
-                        int val = grid[i][j + 1];
-                        int cost = (val == 0 ? 0 : 1);
-
-                        int nc = c + cost;
-                        if (nc <= k) {
-                            dp[i][j + 1][nc] = Math.max(
-                                dp[i][j + 1][nc],
-                                dp[i][j][c] + val
-                            );
-                        }
-                    }
-                }
-            }
-        }
-
-        int ans = -1;
-        for (int c = 0; c <= k; c++) {
-            ans = Math.max(ans, dp[m - 1][n - 1][c]);
-        }
-
-        return ans;
+        int ans = helper(grid, m - 1, n - 1, k, dp);
+        return ans == Integer.MIN_VALUE ? -1 : ans;
     }
+
 }
