04c047e018666b68ca2b7b4f317182376d2de906
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-05-19 18:30:14 +0530

    Time: 2 ms (90.74%), Space: 44.4 MB (14.97%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,43 +1,42 @@
 class Solution {
-    boolean isSafe(int x, int y, int m, int n) {
-        return x >= 0 && x < m && y >= 0 && y < n;
+    boolean isSafe(int m, int n, int x, int y) {
+        if (x < 0 || y < 0 || x >= m || y >= n)
+            return false;
+        return true;
     }
 
     public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        Queue<int[]> pq = new LinkedList<>();
-        int freshOranges = 0;
+        Queue<int[]> q = new LinkedList<>();
+        int m = grid.length, n = grid[0].length;
+        int fresh = 0, minutes = 0;
+        int[][] directions = { { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, 0 } };
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 2)
-                    pq.add(new int[] { i, j });
+                    q.add(new int[] { i, j });
                 else if (grid[i][j] == 1)
-                    freshOranges++;
+                    fresh++;
             }
         }
-        if (freshOranges == 0) return 0;
-        int minutes = 0;
-        int[][] directions = new int[][] { { 0, -1 }, { -1, 0 }, { 0, 1 }, { 1, 0 } };
-        while (!pq.isEmpty()) {
-            int size = pq.size();
-            while (size != 0) {
-                int[] top = pq.poll();
-                int x = top[0];
-                int y = top[1];
+        if (fresh == 0)
+            return 0;
+
+        while (!q.isEmpty() && fresh > 0) {
+            int size = q.size();
+            for (int s = 0; s < size; s++) {
+                int[] cur = q.poll();
                 for (int[] dir : directions) {
-                    int newX = dir[0] + x;
-                    int newY = dir[1] + y;
-                    if (isSafe(newX, newY, m, n) && grid[newX][newY] != 0 && grid[newX][newY] != 2) {
-                        grid[newX][newY] = 2;
-                        pq.add(new int[] { newX, newY });
-                        freshOranges--;
+                    int nx = cur[0] + dir[0], ny = cur[1] + dir[1];
+                    if (isSafe(m, n, nx, ny) && grid[nx][ny] == 1) {
+                        grid[nx][ny] = 2;
+                        fresh--;
+                        q.add(new int[] { nx, ny });
                     }
                 }
-                size--;
             }
             minutes++;
         }
-        return freshOranges == 0 ? minutes - 1 : -1;
+        return fresh == 0 ? minutes : -1;
     }
 }
