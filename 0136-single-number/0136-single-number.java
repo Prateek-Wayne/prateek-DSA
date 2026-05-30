4d3d2a0711771040a7f321537b731ffd30c834b5
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-05-30 11:23:27 +0530

    Time: 4 ms (27.55%), Space: 47.7 MB (5.03%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -2,10 +2,12 @@ class Solution {
     public int singleNumber(int[] nums) {
         Integer xor=null;
         for(int i:nums){
-            if(xor==null)
+            if(xor==null){
                 xor=i;
-            else
+            }
+            else{
                 xor=xor^i;
+            }
         }
         return xor;
     }
