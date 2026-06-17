4acf219f76c21fc90d7146d3a4ee3f10ee2673b3
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-06-17 20:43:56 +0530

    Time: 4 ms (2.4%), Space: 44.7 MB (31.35%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,38 +1,52 @@
 class Solution {
+ int findGollaIndex(int[] nums) {
+        int gollaIndex = -1;
+        int n = nums.length;
+        for (int i = n - 1; i > 0; i--) {
+            if (nums[i - 1] < nums[i]) {
+                gollaIndex = i - 1;
+                break;
+            }
+        }
+        return gollaIndex;
+    }
 
-    public void reverse(int[] nums,int start,int end){
-        while(start<end){
-            int temp=nums[start];
-            nums[start]=nums[end];
-            nums[end]=temp;
+    int findSwapIndex(int[] nums, int gollaIndex) {
+        int n = nums.length;
+        int swapIndex = n - 1;
+        for (int i = n - 1; i > gollaIndex; i--) {
+            if (nums[i] > nums[gollaIndex]) {
+                swapIndex = i;
+                break;
+            }
+        }
+        return swapIndex;
+    }
+
+    void reverse(int[] nums, int gollaIndex) {
+        int start = gollaIndex + 1;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
             start++;
             end--;
         }
         return;
     }
+
     public void nextPermutation(int[] nums) {
-        int golaIndex=-1;
-        int n=nums.length;
-        for(int i=n-1;i>=1;i--){
-            if(nums[i-1]<nums[i]){
-                golaIndex=i-1;
-                break;
-            }
+        int gollaIndex = findGollaIndex(nums);
+        if (gollaIndex != -1) {
+            int swapIndex = findSwapIndex(nums, gollaIndex);
+            System.out.println(gollaIndex);
+            System.out.println(swapIndex);
+            int temp = nums[gollaIndex];
+            nums[gollaIndex] = nums[swapIndex];
+            nums[swapIndex] = temp;
         }
-        if(golaIndex!=-1){
-            int swapIndex=golaIndex+1;
-            for(int i=n-1;i>golaIndex;i--){
-                if(nums[i]>nums[golaIndex]){
-                    swapIndex=i;
-                    break;
-                }
-            }
-            int temp=nums[golaIndex];
-            nums[golaIndex]=nums[swapIndex];
-            nums[swapIndex]=temp;
-        }
-        reverse(nums,golaIndex+1,n-1);
-
-
+        reverse(nums, gollaIndex);
+        return;
     }
 }
