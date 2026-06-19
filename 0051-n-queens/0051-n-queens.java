f1705c530766117a0182e9f091e594123f2aadeb
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-06-19 12:55:38 +0530

    Time: 6 ms (16.36%), Space: 46.5 MB (64.61%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,59 +1,56 @@
 class Solution {
-   boolean isSafe(List<StringBuilder> board, int row, int col) {
-        int newRow = row - 1;
-        int newCol = col - 1;
-        while (newRow >= 0 && newCol >= 0) {
-            if (board.get(newRow).charAt(newCol) == 'Q')
+    public boolean isSafe(List<StringBuilder> board, int i, int j) {
+        int n = board.size();
+        // check row ...
+
+        for (int c = 0; c < j; c++) {
+            if (board.get(i).charAt(c) == 'Q')
                 return false;
-            newRow--;
-            newCol--;
         }
-        newRow = row + 1;
-        newCol = col - 1;
-        while (newRow < board.size() && newCol >= 0) {
-            if (board.get(newRow).charAt(newCol) == 'Q')
+        // upper-left diagonal
+        for (int row = i - 1, col = j - 1; row >= 0 && col >= 0; row--, col--) {
+            if (board.get(row).charAt(col) == 'Q')
                 return false;
-            newRow++;
-            newCol--;
         }
-        newRow = row;
-        newCol = col - 1;
-        while (newCol >= 0) {
-            if (board.get(row).charAt(newCol) == 'Q')
+        // lower-left diagonal
+        for (int row = i + 1, col = j - 1; row < n && col >= 0; row++, col--) {
+            if (board.get(row).charAt(col) == 'Q')
                 return false;
-            newCol--;
         }
         return true;
-
     }
 
-    void helper(int n, List<StringBuilder> board, List<List<String>> ans, int column) {
-        if (column == n) {
-            ans.add(board.stream().map(StringBuilder::toString).toList());
+    public void helper(List<StringBuilder> board, List<List<String>> ans, int col) {
+        int n = board.size();
+        if (n == col) {
+            List<String> temp = new ArrayList<>();
+            for (StringBuilder s : board) {
+                temp.add(s.toString());
+            }
+            ans.add(new ArrayList<>(temp));
             return;
         }
-        // is safe...
         for (int i = 0; i < n; i++) {
-            if (isSafe(board, i, column)) {
-                board.get(i).setCharAt(column, 'Q');
-                helper(n, board, ans, column + 1);
-                board.get(i).setCharAt(column, '.');
+            if (isSafe(board, i, col)) {
+                board.get(i).setCharAt(col, 'Q');
+                helper(board, ans, col + 1);
+                board.get(i).setCharAt(col, '.');
             }
         }
         return;
     }
 
     public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
         List<StringBuilder> board = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            StringBuilder st = new StringBuilder();
-            for (int j = 0; j < n; j++) {
-                st.append('.');
-            }
-            board.add(st);
+            StringBuilder s = new StringBuilder();
+            for (int c = 0; c < n; c++)
+                s.append('.');
+            board.add(s);
         }
-        List<List<String>> ans = new ArrayList<>();
-        helper(n, board, ans, 0);
+
+        helper(board, ans, 0);
         return ans;
     }
 }
