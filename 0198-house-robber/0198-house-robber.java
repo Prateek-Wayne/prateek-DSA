7ae7cc3b4608c7b03c0fd4314111fc6c03592800
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-06-22 10:06:41 +0530

    Time: 0 ms (100%), Space: 42.8 MB (55.42%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,19 +1,18 @@
 class Solution {
-    int helper(int[] nums, int index, int[] dp) {
-        if (index < 0) {
+   public int helper(int[] nums, int index, int[] dp) {
+        if (index >= nums.length)
             return 0;
-        }
         if (dp[index] != -1)
             return dp[index];
-        int left = nums[index] + helper(nums, index - 2, dp);
-        int right = 0 + helper(nums, index - 1, dp);
-        return dp[index] = Math.max(left, right);
+        int pick = nums[index] + helper(nums, index + 2, dp);
+        int notPick = helper(nums, index + 1, dp);
+        return dp[index] = Math.max(pick, notPick);
     }
 
     public int rob(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
         Arrays.fill(dp, -1);
-        return helper(nums, n - 1, dp);
+        return helper(nums, 0, dp);
     }
 }
