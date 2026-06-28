e97e6159517fe2fb78b76aa39ca245c78ab5e5aa
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-06-28 17:02:14 +0530

    Time: 9 ms (67.5%), Space: 77.8 MB (7.5%) - LeetHub

---
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        if (arr[0] != 1) {
+            arr[0] = 1;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] > 1)
+                arr[i] = arr[i - 1] + 1;
+        }
+        return arr[arr.length - 1];
+    }
+}
