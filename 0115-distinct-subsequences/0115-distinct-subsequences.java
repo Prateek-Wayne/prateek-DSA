9f12b37c4b2d0b44d7060ce2a7001b8cdbe38e3c
Prateek Verma <114328592+Prateek-Wayne@users.noreply.github.com>
2026-06-28 18:50:02 +0530

    Time: 20 ms (56.81%), Space: 54.2 MB (64.91%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,25 +1,19 @@
 class Solution {
- int helper(String s, String t, int index1, int index2, Integer[][] dp) {
-        // base...
-        if (index2 < 0)
-            return 1;
-        if (index1 < 0)
-            return 0;
-        if (dp[index1][index2] != null)
-            return dp[index1][index2];
-
-        if (s.charAt(index1) == t.charAt(index2)) {
-            return dp[index1][index2] = helper(s, t, index1 - 1, index2 - 1, dp) + helper(s, t, index1 - 1, index2, dp);
-        } else {
-            return dp[index1][index2] = helper(s, t, index1 - 1, index2, dp);
-        }
-    }
-
     public int numDistinct(String s, String t) {
         int index1 = s.length();
         int index2 = t.length();
-        Integer[][] dp = new Integer[index1][index2];
-
-        return helper(s, t, index1 - 1, index2 - 1, dp);
+        int[][] dp = new int[index1 + 1][index2 + 1];
+        for (int i = 0; i <= index1; i++)
+            dp[i][0] = 1;
+        for (int i = 1; i <= index1; i++) {
+            for (int j = 1; j <= index2; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[index1][index2];
     }
 }
